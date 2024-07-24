c64575962a17fdd1b6009e00d0ebaa73e4629490
Ganesh <143707195+mc095@users.noreply.github.com>
2024-07-24 20:36:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pandas as pd
 from transformers import AutoTokenizer, TapasForQuestionAnswering
 import warnings
 import app
+import app2
 
 # Suppress FutureWarnings
 warnings.filterwarnings("ignore")
@@ -49,31 +51,42 @@ def query_model(question, df, tokenizer, model):
     else:
         return "Sorry, I couldn't find an answer to that question."
 
+# Function to render the Excel QA page
 def render_excel_qa():
     st.header("Ask your Excel 📈")
 
     tokenizer, model = load_model()
     
-    excel_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
-    if excel_file is not None:
-        df = pd.read_excel(excel_file)
-        st.write(df)
+    # Load the default "student_academics" file
+    df = pd.read_excel("student_academics.xlsx")    
 
-        user_question = st.text_input("Ask a question about your Excel: ")
-        if user_question:
-            with st.spinner("Processing..."):
-                result = query_model(user_question, df, tokenizer, model)
-                st.write(result)
+    user_question = st.text_input("Ask a question about the student academics data: ")
+    if user_question:
+        with st.spinner("Processing..."):
+            result = query_model(user_question, df, tokenizer, model)
+            st.write(result)
 
+# Main function to render the app
 def main():
     st.set_page_config(page_title="Excel & Academic Support Chatbot")
     
-    st.sidebar.title("Contents")
-    page = st.sidebar.selectbox("Go to", ["Excel QA", "Academic Support Chatbot"])
+    with st.sidebar:
+        st.title("Academic Support Service")
+        st.write(" Choose an option from the menu below for our services:")
 
-    if page == "Excel QA":
+        selected = option_menu('Your Academic Support',
+                               [
+                                'Your Statistics',
+                                'Excel QA',
+                                'Academic Support Chatbot',
+                               ],
+                                default_index=0)
+
+    if (selected == 'Your Statistics'):
+        app2.main()
+    if (selected == 'Excel QA'):
         render_excel_qa()
-    elif page == "Academic Support Chatbot":
+    if (selected == 'Academic Support Chatbot'):
         app.main()
 
 if __name__ == "__main__":
